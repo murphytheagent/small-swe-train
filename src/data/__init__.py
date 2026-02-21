9e4871354af9b156e633b3cdc1cd48813606c455
Murphy <murphytheagent@gmail.com>
2026-02-21 00:29:30 -0800

    WIP: start SWE trajectory ingestion pipeline

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,10 +2,34 @@
 
 from .feedback_canonicalizer import build_feedback_packet, canonicalize_tool_feedback
 from .tool_schema_adapter import adapt_external_tool_call, map_external_tool
+from .trajectory_ingestion import (
+    Episode,
+    build_episode_from_record,
+    build_episodes,
+    build_training_record,
+    build_training_records,
+    load_qwen_tokenizer,
+    load_raw_records,
+    render_episode_chatml,
+    run_ingestion,
+    tokenize_episode,
+    write_training_records,
+)
 
 __all__ = [
+    "Episode",
     "adapt_external_tool_call",
+    "build_episode_from_record",
+    "build_episodes",
     "build_feedback_packet",
+    "build_training_record",
+    "build_training_records",
     "canonicalize_tool_feedback",
+    "load_qwen_tokenizer",
+    "load_raw_records",
     "map_external_tool",
+    "render_episode_chatml",
+    "run_ingestion",
+    "tokenize_episode",
+    "write_training_records",
 ]
